c86fb77489294bb5f2c40709e5077ae83af53d0c
zhuruihan <ruihanzhu.tiger@gmail.com>
2026-09-09 02:21:09 +0800

    book1-image2

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,43 @@ use console::style;
 use image::{ImageBuffer, RgbImage};
 use indicatif::ProgressBar;
 
+mod vec;
+
+fn ray_color(r: &vec::Ray) -> vec::Point {
+    let unit_dir = r.dir / r.dir.l2norm();
+    let a = (unit_dir.1 + 1.0) * 0.5;
+    vec::Point(1.0, 1.0, 1.0) * (1.0 - a) + vec::Point(0.5, 0.7, 1.0) * a
+}
+
 fn main() {
-    let path = std::path::Path::new("output/book1/image1.png");
+    let path = std::path::Path::new("output/book1/book1-image2.png");
     let prefix = path.parent().unwrap();
     std::fs::create_dir_all(prefix).expect("Cannot create all the parents");
+    
+    // image
+    let aspect_ratio = 16.0 / 9.0;
+    let width = 400;
+    let height = std::cmp::max((width as f64 / aspect_ratio) as u32, 1);
 
-    let width = 256;
-    let height = 256;
+    // camera
+    let focal_length = 1.0;
+    let viewport_height = 2.0;
+    let viewport_width = viewport_height * (width as f64 / height as f64);
+    let camera_center = vec::Point(0.0, 0.0, 0.0);
+    
+    // viewport
+    let viewport_u = vec::Point(viewport_width, 0.0, 0.0);
+    let viewport_v = vec::Point(0.0, -viewport_height, 0.0);
+
+    // viewport delta
+    let pixel_delta_u = viewport_u / width as f64;
+    let pixel_delta_v = viewport_v / height as f64;
+
+    // upper left pixel location
+    let viewport_upper_left = camera_center - vec::Point(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
+    let pixel00_loc = viewport_upper_left + (pixel_delta_u + pixel_delta_v) / 2.0;
+
+
     // different from the book, we use image crate to create a .png image rather than outputting .ppm file, which is not widely used.
     // anyway, you may output any image format you like.
     let mut img: RgbImage = ImageBuffer::new(width, height);
@@ -22,9 +52,14 @@ fn main() {
     for j in (0..height).rev() {
         for i in 0..width {
             let pixel = img.get_pixel_mut(i, j);
-            let r: f64 = (i as f64) / ((width - 1) as f64) * 255.999;
-            let g: f64 = (j as f64) / ((height - 1) as f64) * 255.999;
-            let b: f64 = 0.25 * 255.999;
+            let pixel_center = pixel00_loc + pixel_delta_u * i as f64 + pixel_delta_v * j as f64;
+            let ray_direction = pixel_center - camera_center;
+
+            let ray = vec::Ray::new(camera_center, ray_direction);
+            let pixel_color = ray_color(&ray);
+            let r: f64 = pixel_color.0 * 255.999;
+            let g: f64 = pixel_color.1 * 255.999;
+            let b: f64 = pixel_color.2 * 255.999;
             *pixel = image::Rgb([r as u8, g as u8, b as u8]);
         }
         progress.inc(1);
---
+++ src/vec.rs
@@ -0,0 +1,93 @@
+use std::{ops::{Add, Div, Mul, Neg, Sub}};
+
+#[derive(Debug, Clone, Copy)]  // 添加 Copy
+pub
+struct Point(pub f64, pub f64, pub f64);
+
+impl Point {
+    pub fn l2norm(&self) -> f64 {
+        (self.0.powi(2) + self.1.powi(2) + self.2.powi(2)).sqrt()
+    }
+}
+
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Point(
+            self.0 + rhs.0,
+            self.1 + rhs.1,
+            self.2 + rhs.2
+        )
+    }
+}
+
+impl Sub for Point {
+    type Output = Point;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Point(
+            self.0 - rhs.0,
+            self.1 - rhs.1,
+            self.2 - rhs.2
+        )
+    }
+}
+
+impl Neg for Point {
+    type Output = Point;
+
+    fn neg(self) -> Self::Output {
+        Point(
+            -self.0,
+            -self.1,
+            -self.2
+        )
+    }
+}
+
+impl Mul<f64> for Point {
+    type Output = Point;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        Point(
+            self.0 * rhs,
+            self.1 * rhs,
+            self.2 * rhs
+        )
+    }
+}
+
+impl Div<f64> for Point {
+    type Output = Point;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        Point(
+            self.0 / rhs,
+            self.1 / rhs,
+            self.2 / rhs
+        )
+    }
+}
+
+pub struct Ray {
+    pub orig: Point,
+    pub dir: Point,
+}
+
+impl Ray {
+pub
+    fn new(orig: Point, dir: Point) -> Self {
+        Ray{ orig: orig, dir: dir }
+    }
+    fn new_from_float(x: f64, y: f64, z: f64, dx: f64, dy: f64, dz: f64) -> Self {
+        Ray{ orig: Point(x, y, z), dir: Point(dx, dy, dz) }
+    }
+    fn at(&self, t: f64) -> Point {
+        Point(
+            self.orig.0 + self.dir.0 * t,
+            self.orig.1 + self.dir.1 * t,
+            self.orig.2 + self.dir.2 * t
+        )
+    }
+}
