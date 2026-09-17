c126529692251c1f81d8cca3d6acbd809c2cc11c
zhuruihan <ruihanzhu.tiger@gmail.com>
2026-09-17 16:25:34 +0800

    book1-image5

---
+++ src/hittable.rs
@@ -0,0 +1,116 @@
+use crate::rtweekend::ray;
+use std::{vec};
+
+pub struct HitRecord {
+    pub p: ray::Point,
+    pub normal: ray::Point,
+    pub t: f64,
+    pub front_face: bool,
+}
+
+impl HitRecord {
+    pub fn new() -> Self {
+        HitRecord {
+            p: ray::Point(0.0, 0.0, 0.0),
+            normal: ray::Point(0.0, 0.0, 0.0),
+            t: 0.0,
+            front_face: true
+        }
+    }
+    pub fn set(&mut self, p: ray::Point, normal: ray::Point, t: f64) {
+        self.p = p;
+        self.normal = normal;
+        self.t = t;
+    }
+
+    pub fn set_face_normal(&mut self, r: &ray::Ray, outward_normal: &ray::Point) {
+        self.front_face = r.dir * (*outward_normal) < 0.0;
+        self.normal = if self.front_face {
+            *outward_normal
+        } else {
+            -*outward_normal
+        }
+    }
+}
+
+pub trait Hittable {
+    fn hit(&self, r: &ray::Ray, ray_tmin: f64, ray_tmax: f64, rec: &mut HitRecord) -> bool;
+}
+
+pub struct Sphere {
+    center: ray::Point,
+    radius: f64
+}
+
+impl Sphere {
+    pub fn new(center: ray::Point, radius: f64) -> Self {
+        Sphere { center, radius: radius.max(0.0)}
+    }
+}
+
+impl Hittable for Sphere {
+    fn hit(&self, r: &ray::Ray, ray_tmin: f64, ray_tmax: f64, rec: &mut HitRecord) -> bool {
+        let oc = self.center - r.orig;
+        let a = r.dir.norm_squared();
+        let h = r.dir * oc;
+        let c = oc.norm_squared() - self.radius * self.radius;
+
+        let discriminant = h * h - a * c;
+        if discriminant < 0.0 {
+            return false
+        }
+        let sqrtd = discriminant.sqrt();
+        let mut root = (h - sqrtd) / a;
+        if root < ray_tmin || root > ray_tmax {
+            root = (h + sqrtd) / a;
+            if root < ray_tmin || root > ray_tmax {
+                return false
+            }
+        }
+        
+        let outward_normal = (r.at(root) - self.center) / self.radius;
+        rec.set(r.at(root), (r.at(root) - self.center) / self.radius, root);
+        rec.set_face_normal(r, &outward_normal);
+
+        true
+    }
+}
+
+pub struct HittableList {
+    objects: Vec<Box<dyn Hittable>>,
+}
+
+impl HittableList {
+    pub fn new() -> Self {
+        HittableList { objects: Vec::new() }
+    }
+
+    pub fn new_with_item(object: Box<dyn Hittable>) -> Self {
+        HittableList { objects: vec![object] }
+    }
+
+    pub fn add(&mut self, object: Box<dyn Hittable>) {
+        self.objects.push(object);
+    }
+    
+    pub fn clear(&mut self) {
+        self.objects.clear();
+    }
+}
+
+impl Hittable for HittableList {
+    
+    fn hit(&self, r: &ray::Ray, ray_tmin: f64, ray_tmax: f64, rec: &mut HitRecord) -> bool {
+        let mut hit_anything = false;
+        let mut closest_so_far = ray_tmax;
+        let tmp_rec = rec;
+
+        for obj in &self.objects {
+            if obj.hit(r, ray_tmin, closest_so_far, tmp_rec) {
+                hit_anything = true;
+                closest_so_far = tmp_rec.t;
+            }
+        }
+        hit_anything
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,33 +2,35 @@ use console::style;
 use image::{ImageBuffer, RgbImage};
 use indicatif::ProgressBar;
 
-mod vec;
+use crate::rtweekend::ray::Point;
 
-fn hit_sphere(center: &vec::Point, radius: f64, r: &vec::Ray) -> f64 {
-    let a = r.dir.norm_squared();
-    let b = r.dir * (*center - r.orig) * (-2.0);
-    let c = (*center - r.orig).norm_squared() - radius * radius;
-    let discriminant = b * b - 4.0 * a * c;
-    if discriminant < 0.0 {
-        -1.0
-    } else {
-        (-b - discriminant.sqrt()) / (2.0 * a)
-    }
-}
+pub mod rtweekend;
+pub mod hittable;
 
-fn ray_color(r: &vec::Ray) -> vec::Point {
-    let t = hit_sphere(&vec::Point(0.0, 0.0, -1.0), 0.5, r);
-    if t > 0.0 {
-        let n = (r.at(t) - vec::Point(0.0, 0.0, -1.0)).unit_vector();
-        return vec::Point(n.0 + 1.0, n.1 + 1.0, n.2 + 1.0) * 0.5;
+// fn hit_sphere(center: &rtweekend::ray::Point, radius: f64, r: &rtweekend::ray::Ray) -> f64 {
+//     let a = r.dir.norm_squared();
+//     let h = r.dir * (*center - r.orig);
+//     let c = (*center - r.orig).norm_squared() - radius * radius;
+//     let discriminant = h * h - a * c;
+//     if discriminant < 0.0 {
+//         -1.0
+//     } else {
+//         (-h - discriminant.sqrt()) / a
+//     }
+// }
+
+fn ray_color(r: &rtweekend::ray::Ray, world: &dyn hittable::Hittable) -> rtweekend::ray::Point {
+    let mut tmp_rec = hittable::HitRecord::new();
+    if world.hit(r, 0.0, rtweekend::INFINITY, &mut tmp_rec) {
+        return (tmp_rec.normal + Point(1.0, 1.0, 1.0)) / 2.0
     }
     let unit_dir = r.dir.unit_vector();
     let a = (unit_dir.1 + 1.0) * 0.5;
-    vec::Point(1.0, 1.0, 1.0) * (1.0 - a) + vec::Point(0.5, 0.7, 1.0) * a
+    rtweekend::ray::Point(1.0, 1.0, 1.0) * (1.0 - a) + rtweekend::ray::Point(0.5, 0.7, 1.0) * a
 }
 
 fn main() {
-    let path = std::path::Path::new("output/book1/image4.png");
+    let path = std::path::Path::new("output/book1/image5.png");
     let prefix = path.parent().unwrap();
     std::fs::create_dir_all(prefix).expect("Cannot create all the parents");
 
@@ -41,11 +43,11 @@ fn main() {
     let focal_length = 1.0;
     let viewport_height = 2.0;
     let viewport_width = viewport_height * (width as f64 / height as f64);
-    let camera_center = vec::Point(0.0, 0.0, 0.0);
+    let camera_center = rtweekend::ray::Point(0.0, 0.0, 0.0);
 
     // viewport
-    let viewport_u = vec::Point(viewport_width, 0.0, 0.0);
-    let viewport_v = vec::Point(0.0, -viewport_height, 0.0);
+    let viewport_u = Point(viewport_width, 0.0, 0.0);
+    let viewport_v = Point(0.0, -viewport_height, 0.0);
 
     // viewport delta
     let pixel_delta_u = viewport_u / width as f64;
@@ -53,7 +55,7 @@ fn main() {
 
     // upper left pixel location
     let viewport_upper_left =
-        camera_center - vec::Point(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
+        camera_center - rtweekend::ray::Point(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
     let pixel00_loc = viewport_upper_left + (pixel_delta_u + pixel_delta_v) / 2.0;
 
     // different from the book, we use image crate to create a .png image rather than outputting .ppm file, which is not widely used.
@@ -66,14 +68,17 @@ fn main() {
         ProgressBar::new((height * width) as u64)
     };
 
+    let mut world = hittable::HittableList::new();
+    world.add(Box::new(hittable::Sphere::new(Point(0.0, 0.0, -1.0), 0.5)));
+    world.add(Box::new(hittable::Sphere::new(Point(0.0, -100.5, -1.0), 100.0)));
     for j in (0..height).rev() {
         for i in 0..width {
             let pixel = img.get_pixel_mut(i, j);
             let pixel_center = pixel00_loc + pixel_delta_u * i as f64 + pixel_delta_v * j as f64;
             let ray_direction = pixel_center - camera_center;
 
-            let ray = vec::Ray::new(camera_center, ray_direction);
-            let pixel_color = ray_color(&ray);
+            let ray = rtweekend::ray::Ray::new(camera_center, ray_direction);
+            let pixel_color = ray_color(&ray, &world);
             let r: f64 = pixel_color.0 * 255.999;
             let g: f64 = pixel_color.1 * 255.999;
             let b: f64 = pixel_color.2 * 255.999;
